61e64744913e86eac005e0aa0dee6460bcdc5dee
CodingWithPandeyji <mohitpandey.cs19@gmail.com>
2024-05-16 22:11:47 +0530

    changes

--- scene/scripts.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.querySelector('.nav-bar'); // Corrected selector
-const toggleButton = document.getElementById('toggle-button');
-
-toggleButton.addEventListener('click', () => {
-    navbar.classList.toggle('acti');
-});
-
